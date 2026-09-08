97a3faf799f8bc96a6462ca92eb8a33b2c31596e
gabriel-ricardo-ADS <gabrielricardoglima@gmail.com>
2026-09-07 23:03:05 -0300

    feat: Adicionando lista de palavras e função de sorteio

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+from palavras import lista_palavras, sortear_palavras
+
+
 def main():
     print("=" * 40)
     print("          CODENAMES - PYTHON")
@@ -6,6 +9,11 @@ def main():
     print("\nBem-vindo ao Codenames!")
 
     input("\nPressione Enter para iniciar...")
+
+    palavras_partida = sortear_palavras(lista_palavras, 25)
+
+    print("\nPalavras sorteadas:")
+    print(palavras_partida)
 
 
 if __name__ == "__main__":

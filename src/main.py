8c886236dc77e6d190312d4eb56cd33414bd7ab9
anapmagalhaess <bruclaramagalhaes2015@gmail.com>
2026-09-09 11:34:53 -0300

    feat: Adição das funções 'cadastrar_jogadores, dividir_equipes, escolher_mestre, definir_equipe_inicial' e integração a função main.

--- src/main.py
+++ src/main.py
@@ -1,15 +1,32 @@
 from palavras import lista_palavras, sortear_palavras
 from tabuleiro import estrutura_tabuleiro, gerar_categorias, exibir_tabuleiro
 from tabuleiro import revelar_palavra
+from jogadores import cadastrar_jogadores, dividir_equipes, escolher_mestre, definir_equipe_inicial
+
+VERDE = "\033[32m"
+RESET = "\033[0m"
 
 def main():
     print("=" * 40)
-    print("          CODENAMES - PYTHON")
+    print(f"          CODENAMES - {VERDE}PYTHON{RESET}")
     print("=" * 40)
 
-    print("\nBem-vindo ao Codenames!")
+    print(f"\nBem-vindo ao Codenames!")
 
     input("\nPressione Enter para iniciar...")
+
+    #Cadastro dos jogadores
+    jogadores = cadastrar_jogadores()
+
+    #Divisao das equipes
+    equipe_vermelha, equipe_azul = dividir_equipes(jogadores)
+
+    #Escolha dos mestres-espioes
+    mestre_vermelho = escolher_mestre(equipe_vermelha, "VERMELHA")
+    mestre_azul = escolher_mestre(equipe_azul, "AZUL")
+
+    #Quem inicia o jogo
+    equipe_inicial = definir_equipe_inicial()
 
     palavras_partida = sortear_palavras(lista_palavras, 25)
     categorias_geradas = gerar_categorias(False, True)
